ef91621fb618b071e3d75f9dbc1f8fbc72ceb96d
Nicholas Otieno <niklasoti@gmail.com>
2023-02-26 20:42:00 +0100

    Exercise 2a

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	fmt.Println("Hello World")
+
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.\n", "Nicholas", "Stockholm", 10, true)
+}
